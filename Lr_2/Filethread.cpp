8335feacdbc5aea61cb9d730669c93da720dc4c6
NatalyaLega <lega.nata@bk.ru>
2022-10-13 15:24:14 +0300

    all work

--- Lr_2/Filethread.cpp
+++ Lr_2/Filethread.cpp
@@ -32,7 +32,7 @@ void Filethread::scan_text()
 	int flag4 = 0;//флаг что не после букв
 	while (1)
 	{//Цикл для проверки правильности введенной команды в меню {
-		cout << "1 - Чтение из из фала строк, содержащих 2значные числа\n2 - Выход\n\n";//Информация для пользователя
+		cout << "1 - Чтение из из фала строк, НЕ содержащих 2значные числа\n2 - Чтение из файла строк, содержащих 2значные числа\n3 - Выход\n";//Информация для пользователя
 		cout << "Выбирете команду:\n";
 		
 		cin >> choise;
@@ -55,6 +55,110 @@ void Filethread::scan_text()
 					throw err;
 				}
 				           ///ОСНОВНАЯ ЛОГИКА///
+				while (((ch = in.get()) != EOF)) //пока не конец файла
+				{
+					size++;
+					if (ch >= '0' && ch <= '9')
+					{
+						flag1 = 1;//Сейчас цифра
+						cnt++;
+					}
+					else
+					{
+						if (flag2 == 1 && (ch == ' ' || ch == '\n'))
+						{
+							flag3 = 1;  //значит, что 2 числа и потом пропуск или конец строки
+							cnt = 0; //обнуляем счетчик
+							flag1 = 0;
+							flag2 = 0;
+							flag4 = 0;
+						}
+						else
+						{
+							if (ch == ' ')
+								flag4 = 0; //закончилась послед-ть из 1го
+							else
+								flag4 = 1; //не закончилась
+							flag2 = 0;
+							flag1 = 0;
+							cnt = 0;
+						}
+					}
+
+					if (flag1 == 1 && cnt == 2 && flag4 == 0) //2 цифры подряд
+					{
+						flag2 = 1;
+					}
+					if (flag1 == 1 && cnt == 3)//3 цифры подряд
+					{
+						flag1 = 0;
+						flag2 = 0;
+						cnt = 0;
+						flag4 = 0;
+					}
+					if (flag4 == 0)
+					{
+						mas[size - 1] = '\0';
+						cout << mas << "\n";
+						cnt = 0;
+						flag1 = flag2 = flag3 = flag4 = 0;
+
+						for (int i = 0; i < size; i++)
+						{
+							mas[i] = 0;
+						}
+						size = 0;
+						continue;
+					}
+					else if (ch == '\n')
+					{
+						flag1 = flag2 = flag3 = flag4 = 0;
+						for (int i = 0; i < size; i++)
+						{
+							mas[i] = 0;
+						}
+						size = 0;
+					}
+					if (size != 0)
+						mas[size - 1] = ch;
+
+				}
+				in.close();
+			}
+			catch (exception& err)
+			{
+				cout << err.what() << endl << endl;
+
+				in.close();
+				cout << "\nИсходный файл закрыт.\n";
+			}
+			if (flag4 == 0)
+			{
+				mas[size] = '\0';
+				cout << mas << "\n";
+				cnt = 0;
+				flag1 = flag2 = flag3 = flag4 = 0;
+			}
+			cout << "\nНажмите любую кнопку...\n";
+			_getch();
+			//return 0;
+			break;
+		case 2:
+			system("cls");
+			try
+			{
+				in.open("test.txt");
+				if (!in)//Ошибка открытия файла
+				{
+					exception err("Ошибка открытия файла\n");
+					throw err;
+				}
+				if (in.peek() == -1)
+				{
+					exception err("\nОткрываемый файл пустой\n");
+					throw err;
+				}
+				///ОСНОВНАЯ ЛОГИКА///
 				while (((ch = in.get()) != EOF)) //пока не конец файла
 				{
 					size++;
@@ -143,11 +247,9 @@ void Filethread::scan_text()
 			_getch();
 			//return 0;
 			break;
-		case 2:
-			cout << "\nПрограмма заверешена...\n";
+		case 3:
+			cout << "\n Программа завершена..\n";
 			_getch();
-			
-			break;
 
 		}
 	}
